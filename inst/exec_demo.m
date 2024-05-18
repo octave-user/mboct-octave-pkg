e2b426604f9d62e878c36c4e1e12ba2604f6acfe
Reinhard <octave-user@a1.net>
2024-05-18 22:34:57 +0200

    Ensure compatibility with Octave's test-function

--- inst/exec_demo.m
+++ inst/exec_demo.m
@@ -29,7 +29,7 @@
 ##
 ## @end deftypefn
 
-function status = exec_demo(filename_in, index, first_line, type)
+function status = exec_demo(__file, index, first_line, type)
   if (nargin < 1 || nargin > 4)
     print_usage();
   endif
@@ -45,7 +45,7 @@ function status = exec_demo(filename_in, index, first_line, type)
   if (nargin < 4)
     type = "demo";
   endif
-  
+
   fid_in = -1;
   fid_out = -1;
   line_no = 0;
@@ -53,60 +53,60 @@ function status = exec_demo(filename_in, index, first_line, type)
 
   unwind_protect
     temp_file_name = strcat(tempname(), ".m");
-    
+
     unwind_protect
-      [DIR, NAME, EXT] = fileparts(filename_in);
-      
+      [DIR, NAME, EXT] = fileparts(__file);
+
       if (length(EXT) == 0)
-        filename_in = strcat(filename_in, ".m");
+        __file = strcat(__file, ".m");
       endif
-      
-      [fid_in, msg] = fopen(filename_in, "rt");
-      
+
+      [fid_in, msg] = fopen(__file, "rt");
+
       if (-1 == fid_in)
-        error("Failed to open file \"%s\": %s", filename_in, msg);
+        error("Failed to open file \"%s\": %s", __file, msg);
       endif
-      
+
       [fid_out, msg] = fopen(temp_file_name, "wt");
-      
+
       if (-1 == fid_out)
         error("Failed to open file \"%s\": %s", temp_file_name, msg);
       endif
-      
+
       f_demo_found = 0;
-      
+
       while (true)
         line = fgets(fid_in);
-        
+
         if (-1 == line)
           break;
         endif
-        
+
         ++line_no;
-        
+
         if (strncmp(line, sprintf("%%!%s", type), 6) || strncmp(line, sprintf("%%!x%s", type), 7))
           if (++f_demo_found == index)
             break;
           endif
         endif
       endwhile
-      
+
       if (f_demo_found == index)
-        fprintf(stderr, "Commands from demo %d of file \"%s\" are written to \"%s\" ...\n", f_demo_found, filename_in, temp_file_name);
-        
+        fprintf(stderr, "Commands from demo %d of file \"%s\" are written to \"%s\" ...\n", f_demo_found, __file, temp_file_name);
+
         while ( 1 == 1 )
           line = fgets(fid_in);
-          
+
           if ( -1 == line )
             break;
           endif
-          
+
           ++line_no;
-          
+
           if (strncmp(line, "%!demo", 6) || strncmp(line, "%!test", 6) || strncmp(line, "%!xtest", 7))
             break;
           endif
-          
+
           if (strncmp(line, "%!", 2))
             if (line_no >= first_line)
               line = line(3:end);
@@ -116,7 +116,7 @@ function status = exec_demo(filename_in, index, first_line, type)
           endif
         endwhile
       endif
-      
+
     unwind_protect_cleanup
       if (fid_in != -1)
         fclose(fid_in);
@@ -125,7 +125,7 @@ function status = exec_demo(filename_in, index, first_line, type)
         fclose(fid_out);
       endif
     end_unwind_protect
-    
+
     try
       fprintf(stderr, "\nfile \"%s\" will be executed!\n", temp_file_name);
       evalin("base", sprintf("clear all; source('%s')", temp_file_name));
@@ -138,4 +138,3 @@ function status = exec_demo(filename_in, index, first_line, type)
     [~] = unlink(temp_file_name);
   end_unwind_protect
 endfunction
-
